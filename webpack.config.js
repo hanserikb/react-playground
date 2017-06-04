c4ea8c8095ec33acb332e62e785c44d5688b5bb2
Hans Bentlov <hanserikb@gmail.com>
2017-06-04 13:55:17 +0200

    Babysteps

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
