460ed8ad2dfc2a810d348958ccb8627b0f3c224c
Hans Bentlov <hanserikb@gmail.com>
2017-06-04 15:53:23 +0200

    Load data from an API

--- app/index.js
+++ app/index.js
@@ -6,22 +6,54 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Hans'
+      name: 'Available freerider cars',
+      loading: true,
+      rides: []
     };
+  }
+  stopLoading() {
+    this.setState(Object.assign({}, this.state, { loading: false }));
+  }
+  setRides(rides) {
+    this.setState(Object.assign({}, this.state, {
+      rides: rides
+    }));
+  }
+  fetchRides() {
+    return fetch('http://localhost:3001/')
+      .then(res => res.json())
+      .then(rides => this.setRides(rides))
+      .then(this.stopLoading.bind(this))
+      .catch((err) => console.error('Something wrong happened: ', err));
+  }
+  componentDidMount() {
+    setTimeout(this.fetchRides.bind(this), 1000);
   }
   render() {
     return (
     <div>
-      <h1>Hello</h1>
-      <SubComponent text={this.state.name}/>
+      <h1>{this.state.name}</h1>
+      <Rides rides={this.state.rides} loading={this.state.loading}/>
     </div>
     );
   }
 }
 
-const SubComponent = props => <h2>Hi {props.text}</h2>;
-SubComponent.propTypes = {
-  text: PropTypes.string.isRequired
+const Rides = props => (
+  <div>
+    <h2>{ props.loading ? 'Loading..' : '' }</h2>
+    <ul>
+      { props.rides.map((ride, i) => (
+        <li key={`ride_${i}`}>
+          <h3>{ride.from.name} - {ride.to.name}</h3>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+Rides.propTypes = {
+  rides: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 
