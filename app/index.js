2d374b49f40f6c47592e35daf2c142e86c36bbd7
Hans Bentlov <hanserikb@gmail.com>
2017-06-04 13:59:03 +0200

    Add a proptype

--- app/index.js
+++ app/index.js
@@ -2,9 +2,27 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      name: 'Hans'
+    }
+  },
   render: function() {
-    return <h1>Hello</h1>;
+    return (
+    <div>
+      <h1>Hello</h1>
+      <SubComponent text={this.state.name}/>
+    </div>
+    );
   }
 });
+
+var SubComponent = function(props) {
+  return <h2>Hi {props.text}</h2>
+}
+
+SubComponent.propTypes = {
+  text: React.PropTypes.string.isRequired
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'));
