2ddf5941486edc3e753be5fc393407dd42715ef2
Hans Bentlov <hanserikb@gmail.com>
2017-06-04 20:54:19 +0200

    Use the new Map component. Add classes for styling.  Add dates for booking

--- app/Rides/index.js
+++ app/Rides/index.js
@@ -1,15 +1,21 @@
-var React = require('react');
-var PropTypes = require('prop-types');
+const React = require('react');
+const PropTypes = require('prop-types');
+const moment = require('moment');
+const Map = require('../Map');
 
 const Rides = props => (
-  <div>
-    <h2>{ props.loading ? 'Loading..' : '' }</h2>
-    <ul>
-      { props.rides.map((ride, i) => (
-        <li key={`ride_${i}`}>
-          <h3>{ride.from.name} - {ride.to.name}</h3>
-        </li>
-      ))}
+  <div className="rides">
+    { props.loading ? <h2>Loading..</h2> : '' }
+    <ul className="rides--list">
+      { props.rides.map((ride, i) => {
+        let startDate = moment(ride.startData).format('MMMM Do');
+        let endDate = moment(ride.endData).format('MMMM Do');
+        let dateStr = startDate == endDate ? startDate : `${startDate} to ${endDate}`;
+        return (
+        <li className="rides--listitem" key={`ride_${i}`}>
+          <span className="rides--listitem--text">{dateStr} | {ride.from.name} - {ride.to.name} | {ride.car} </span><Map latLng={[ride.from.station.location.latLng, ride.to.station.location.latLng]} />
+        </li>)
+      })}
     </ul>
   </div>
 );
