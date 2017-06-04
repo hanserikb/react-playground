e9328fa1637e03981a902ad38dbe4809b8499095
Hans Bentlov <hanserikb@gmail.com>
2017-06-05 00:24:57 +0200

    Do some sorting

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const ReactDOM = require('react-dom');
 const Rides = require('./Rides');
 const styles = require('./styles/styles.css');
 const RideService = require('./RideService');
-
+const moment = require('moment')
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,12 +23,16 @@ class App extends React.Component {
   }
   fetchRides() {
     return RideService.fetch()
+      .then(rides => rides.sort(ride => {
+        return (moment(ride.startData).isSame(new Date(), "day") &&
+          moment(ride.endData).isSame(new Date(), "day")) ? -1 : 1;
+      }))
       .then(rides => this.setRides(rides))
       .then(this.stopLoading.bind(this))
       .catch((err) => console.error('Something wrong happened: ', err));
   }
   componentDidMount() {
-    setTimeout(this.fetchRides.bind(this), 1000);
+    setTimeout(this.fetchRides.bind(this), 500);
   }
   render() {
     return (
